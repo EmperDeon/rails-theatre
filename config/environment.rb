9467eeda51b81921fdbfe1d6fab787a3b71ac8d3
ilz2010 <ilz2010@yandex.ru>
2017-01-17 13:44:17 +0500

    Heroku fix.5

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,6 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Rails.logger = Le.new('dc1c6cbf-9eab-4b54-aaf2-80b307f48e50', :debug => true, :local => true)
-
-Rails.logger.warn("Look at me, I'm a warning message")
-Rails.logger.info("Look at me, I'm an info message")
-Rails.logger.debug("Look at me, I'm a debug message")
+if Rails.env == 'production'
+    Rails.logger = Le.new('dc1c6cbf-9eab-4b54-aaf2-80b307f48e50', :debug => true, :local => true)
+end
--- db/seeds/init_seeder.rb
+++ db/seeds/init_seeder.rb
@@ -1,7 +1,9 @@
 # Set local MySQL increments to ClearDB's
 #
 
-exec_sql 'SET @@global.auto_increment_increment = 10'
-exec_sql 'SET @@global.auto_increment_offset = 4'
-exec_sql 'SET @@session.auto_increment_increment = 10'
-exec_sql 'SET @@session.auto_increment_offset = 4'
+if Rails.env == 'development'
+    exec_sql 'SET @@global.auto_increment_increment = 10'
+    exec_sql 'SET @@global.auto_increment_offset = 4'
+    exec_sql 'SET @@session.auto_increment_increment = 10'
+    exec_sql 'SET @@session.auto_increment_offset = 4'
+end
