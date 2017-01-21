6c3b9e31f5dd8ac02d41003895cac0cc560fb181
ilz2010 <ilz2010@yandex.ru>
2017-01-21 19:13:35 +0500

    FIX for - Deleted link to hall from index Thinking about using link_to in place of tag a in *.slim

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-if Rails.env == 'production'
-    Rails.logger = Le.new('dc1c6cbf-9eab-4b54-aaf2-80b307f48e50', :debug => true, :local => true)
-end
